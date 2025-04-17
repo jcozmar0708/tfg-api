ebdba8189966b72b136f9e7dc8bb8e7f7f8cc4f3
José Manuel Cózar <josemanueltrafu@gmail.com>
2025-04-17 14:21:18 +0200

    Architecture and configuration changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
-import { GroupsModule } from './groups/groups.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { ConfigModule } from '@nestjs/config';
+import { UsersService } from './user/user.service';
+import { UsersController } from './user/user.controller';
+import { UsersModule } from './user/user.module';
+import { GroupsService } from './group/group.service';
+import { GroupsController } from './group/group.controller';
+import { GroupsModule } from './group/group.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI ?? ''),
     UsersModule,
-    GroupsModule
-  ]
+    GroupsModule,
+  ],
+  providers: [UsersService, GroupsService],
+  controllers: [UsersController, GroupsController],
 })
-export class AppModule { }
+export class AppModule {}
