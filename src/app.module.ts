0c8fe198979b5c88afd36d01e622179cdfd38796
José Manuel Cózar <josemanueltrafu@gmail.com>
2025-05-05 21:30:10 +0200

    Dependencies and config changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
-import { UsersService } from './user/user.service';
-import { UsersController } from './user/user.controller';
 import { UsersModule } from './user/user.module';
-import { GroupsService } from './group/group.service';
-import { GroupsController } from './group/group.controller';
 import { GroupsModule } from './group/group.module';
 
 @Module({
@@ -18,7 +14,5 @@ import { GroupsModule } from './group/group.module';
     UsersModule,
     GroupsModule,
   ],
-  providers: [UsersService, GroupsService],
-  controllers: [UsersController, GroupsController],
 })
 export class AppModule {}
