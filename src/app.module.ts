ba95ceb900c92fa3da26cb83f593b4dad8fe83e5
Jose Manuel Cozar Martinez <jcozmar0708@g.educaand.es>
2025-05-31 23:13:27 +0200

    SSL signing is disabled in node for development

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,9 @@ import { AuthModule } from './auth/auth.module';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true,
       }),
       inject: [ConfigService],
     }),
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ConfigService } from '@nestjs/config';
 import { getConstants } from './common/constants';
 import { ValidationPipe } from '@nestjs/common';
 
+if (process.env.NODE_ENV !== 'production') {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
