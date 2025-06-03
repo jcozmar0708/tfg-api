1f9c9fef0c5adf778c1270824064d5f225d36b5b
José Manuel Cózar <josemanueltrafu@gmail.com>
2025-06-03 22:33:03 +0200

    Added new environment and dependency constants to verify invitation token

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -12,6 +12,9 @@ const getEnvString = (key: string, configService: ConfigService): string => {
 export const getConstants = (configService: ConfigService) => ({
   PORT: parseInt(getEnvString('PORT', configService)),
 
+  CORS_ORIGIN: getEnvString('CORS_ORIGIN', configService),
+  FRONT_URL: getEnvString('FRONT_URL', configService),
+
   MAIL_HOST: getEnvString('MAIL_HOST', configService),
   MAIL_PORT: parseInt(getEnvString('MAIL_PORT', configService)),
   MAIL_USER: getEnvString('MAIL_USER', configService),
